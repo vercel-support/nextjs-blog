c9c92b515dce24f3383dd2ec14a61b102737435a
Keyur <keyur.raval18@gmail.com>
2021-02-10 21:06:11 +0530

    Add next config file

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async rewrites() {
+    return [
+      // we need to define a no-op rewrite to trigger checking
+      // all pages/static files before we attempt proxying
+      {
+        source: '/nginx/:path*',
+        destination: `https://romantic-visvesvaraya-39e8bf.netlify.app/nginx/:path*`,
+      },
+    ]
+  },
+}
