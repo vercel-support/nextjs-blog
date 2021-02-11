4fcfb7c2c0f00865419d4c3cb6b7f0ef8267af11
Keyur <keyur.raval18@gmail.com>
2021-02-11 11:05:23 +0530

    Remove netlify

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ module.exports = {
       // we need to define a no-op rewrite to trigger checking
       // all pages/static files before we attempt proxying
       {
-        source: '/nginx/:path*',
-        destination: `https://romantic-visvesvaraya-39e8bf.netlify.app/nginx/:path*`,
+        source: '/nginx/:match*',
+        destination: `https://romantic-visvesvaraya-39e8bf.netlify.app/nginx/:match*`,
       },
     ]
   },
