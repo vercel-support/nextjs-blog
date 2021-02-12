5f6481c709e510e8c1ce9a5ac5ef071239800f9f
Keyur <keyur.raval18@gmail.com>
2021-02-12 20:13:03 +0530

    Remove nextconfig

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-module.exports = {
-  async rewrites() {
-    return [
-      // we need to define a no-op rewrite to trigger checking
-      // all pages/static files before we attempt proxying
-      {
-        source: '/nginx/:match*',
-        destination: `https://romantic-visvesvaraya-39e8bf.netlify.app/nginx/:match*`,
-      },
-    ]
-  },
-}
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       // we need to define a no-op rewrite to trigger checking
+//       // all pages/static files before we attempt proxying
+//       {
+//         source: '/nginx/:match*',
+//         destination: `https://romantic-visvesvaraya-39e8bf.netlify.app/nginx/:match*`,
+//       },
+//     ]
+//   },
+// }
