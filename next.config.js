11adb9302c17441965c1d74d09d25004901e3c8b
Keyur <keyur.raval18@gmail.com>
2021-02-17 12:17:15 +0530

    Removed vercel json and used next config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async rewrites() {
+    return [
+      // Rewriting to an external URL
+      {
+        source: '/nginx/:slug*',
+        destination: 'https://romantic-visvesvaraya-39e8bf.netlify.app/nginx/:slug*',
+      },
+    ]
+  },
+}
